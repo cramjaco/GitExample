0b754a72ef5e695597745a854613c4c956f4911d
Jacob A Cram <jcram@umces.edu>
2021-04-23 11:46:08 -0400

    Made a change

--- Thing1.R
+++ Thing1.R
@@ -1,4 +1,5 @@
 # Here is some R code
 library(tidyverse)
 
-ggplot(aes(x = Petal.Length, y = Petal.Width), data = iris) + geom_point()
+ggplot(aes(x = Petal.Length, y = Petal.Width), data = iris) + geom_point() +
+geom_line()
